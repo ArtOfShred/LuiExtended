661dc0c6c5402612368c54f5622cf4fb4fa925ca
psypanda <c0re@psypanda.org>
2017-02-27 17:09:24 +0100

    Formatting

--- i18n/en.lua
+++ i18n/en.lua
@@ -1,58 +1,43 @@
 -- Chat Announcement Component
-ZO_CreateStringId("SI_LUIE_CA_GUILD_MEMBER_ADDED",          "|cFEFEFE<<1>>|r has joined <<2>>")
-ZO_CreateStringId("SI_LUIE_CA_GUILD_MEMBER_REMOVED",        "|cFEFEFE<<1>>|r has left <<2>>")
-
-ZO_CreateStringId("SI_LUIE_CA_GUILD_MOTD_CHANGED",          "The message of the day for <<1>> has changed:\n<<2>>")
-
-ZO_CreateStringId("SI_LUIE_CA_GUILD_RANK_CHANGED",          "|cFEFEFE<<1>>|r's rank in <<2>> has been changed to <<3>>")
-ZO_CreateStringId("SI_LUIE_CA_GUILD_RANK_CHANGED_SELF",     "You have been <<1>> to <<2>> in <<3>>")
-
-ZO_CreateStringId("SI_LUIE_CA_GUILD_JOIN_SELF",             "You have joined <<1>>")
-ZO_CreateStringId("SI_LUIE_CA_GUILD_LEAVE_SELF",            "You have left <<1>>")
-
-ZO_CreateStringId("SI_LUIE_CA_GUILD_INVITE_SELF",           "|cFEFEFE<<1>>|r has invited you to join <<2>>")
-
-ZO_CreateStringId("SI_LUIE_CA_FRIEND_ADDED",                "|cFEFEFE<<1>>|r added to friends list.")
-ZO_CreateStringId("SI_LUIE_CA_FRIEND_REMOVED",              "|cFEFEFE<<1>>|r removed from friends list.")
-ZO_CreateStringId("SI_LUIE_CA_FRIEND_INVITE_PENDING",       "|cFEFEFE<<1>>|r wants to be your friend.")
-
-ZO_CreateStringId("SI_LUIE_CA_QUEST_SHARE_MSG",             "|cFEFEFE<<1>>|r wants to share the quest: <<2>>")
-
-ZO_CreateStringId("SI_LUIE_CA_READY_CHECK_ACTIVITY",        "|cFFFFFF<<1>>|r ready.")
-
-ZO_CreateStringId("SI_LUIE_CA_VOTE_NOTIFY_VOTEKICK_START",  "A vote to kick |cFEFEFE<<1>>|r from the group has started.")
-ZO_CreateStringId("SI_LUIE_CA_VOTE_NOTIFY_VOTEKICK_FAIL",   "A vote to kick |cFEFEFE<<1>>|r from the group has failed.")
-
-ZO_CreateStringId("SI_LUIE_CA_GROUP_INVITE_RECEIVED",       "|cFEFEFE<<1>>|r has invited you to join a group.")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_LEADER_CHANGED",        "|cFEFEFE<<1>>|r is now the group leader!")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_JOIN",           "|cFEFEFE<<1>>|r has joined the group.")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_LEAVE",          "|cFEFEFE<<1>>|r has left the group.")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_LEAVE_SELF",     "You have left the group.")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_KICKED",         "|cFEFEFE<<1>>|r has been removed from the group.")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_KICKED_SELF",    "You have been removed from the group.")
-ZO_CreateStringId("SI_LUIE_CA_GROUP_DISBAND_MSG",           "The group has been disbanded.")
-
-ZO_CreateStringId("SI_LUIE_CA_TRADE_INVITE_MSG_SELF",       "You have invited |cFEFEFE<<1>>|r to trade.")
-ZO_CreateStringId("SI_LUIE_CA_TRADE_INVITE_MSG",            "|cFEFEFE<<1>>|r has invited you to trade.")
-
-ZO_CreateStringId("SI_LUIE_CA_CANT_THINK_OF_NAME_MSG1",     "<<1>> and <<2>> gold.")
-ZO_CreateStringId("SI_LUIE_CA_CANT_THINK_OF_NAME_MSG2",     "<<1>> and gold.")
-
-ZO_CreateStringId("SI_LUIE_CA_MAIL_RECEIVED_GOLD_MSG",      "Received mail with gold.")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_RECEIVED_VAR_GOLD_MSG",  "Received mail with <<1>> gold.")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_GOLD_SENT",          "COD Payment sent!")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_VAR_GOLD_SENT1",     "COD Payment of <<1>> gold sent!")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_VAR_GOLD_SENT2",     "COD sent for <<1>> gold!")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_SENT_SUCCESS",           "Mail sent!")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_SENT_SUCCESS",       "COD sent!")
-ZO_CreateStringId("SI_LUIE_CA_MAIL_SENT_VAR_GOLD_MSG",      "Mail sent with <<1>> gold!")
-
-ZO_CreateStringId("SI_LUIE_CA_XP_LVL_ANNOUNCE1",            "You have reached <<1>><<2>>")
-ZO_CreateStringId("SI_LUIE_CA_XP_LVL_ANNOUNCE2",            "You have reached <<1>><<2>> <<3>>")
-
-ZO_CreateStringId("SI_LUIE_CA_CP_LVL_ANNOUNCE1",            "Champion Level Achieved! <<1>><<2>>")
-ZO_CreateStringId("SI_LUIE_CA_CP_LVL_ANNOUNCE2",            "Champion Level Achieved! <<1>><<2>> <<3>>")
-
+ZO_CreateStringId("SI_LUIE_CA_GUILD_MEMBER_ADDED",                      "|cFEFEFE<<1>>|r has joined <<2>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_MEMBER_REMOVED",                    "|cFEFEFE<<1>>|r has left <<2>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_MOTD_CHANGED",                      "The message of the day for <<1>> has changed:\n<<2>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_RANK_CHANGED",                      "|cFEFEFE<<1>>|r's rank in <<2>> has been changed to <<3>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_RANK_CHANGED_SELF",                 "You have been <<1>> to <<2>> in <<3>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_JOIN_SELF",                         "You have joined <<1>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_LEAVE_SELF",                        "You have left <<1>>")
+ZO_CreateStringId("SI_LUIE_CA_GUILD_INVITE_SELF",                       "|cFEFEFE<<1>>|r has invited you to join <<2>>")
+ZO_CreateStringId("SI_LUIE_CA_FRIEND_ADDED",                            "|cFEFEFE<<1>>|r added to friends list.")
+ZO_CreateStringId("SI_LUIE_CA_FRIEND_REMOVED",                          "|cFEFEFE<<1>>|r removed from friends list.")
+ZO_CreateStringId("SI_LUIE_CA_FRIEND_INVITE_PENDING",                   "|cFEFEFE<<1>>|r wants to be your friend.")
+ZO_CreateStringId("SI_LUIE_CA_QUEST_SHARE_MSG",                         "|cFEFEFE<<1>>|r wants to share the quest: <<2>>")
+ZO_CreateStringId("SI_LUIE_CA_READY_CHECK_ACTIVITY",                    "|cFFFFFF<<1>>|r ready.")
+ZO_CreateStringId("SI_LUIE_CA_VOTE_NOTIFY_VOTEKICK_START",              "A vote to kick |cFEFEFE<<1>>|r from the group has started.")
+ZO_CreateStringId("SI_LUIE_CA_VOTE_NOTIFY_VOTEKICK_FAIL",               "A vote to kick |cFEFEFE<<1>>|r from the group has failed.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_INVITE_RECEIVED",                   "|cFEFEFE<<1>>|r has invited you to join a group.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_LEADER_CHANGED",                    "|cFEFEFE<<1>>|r is now the group leader!")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_JOIN",                       "|cFEFEFE<<1>>|r has joined the group.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_LEAVE",                      "|cFEFEFE<<1>>|r has left the group.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_LEAVE_SELF",                 "You have left the group.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_KICKED",                     "|cFEFEFE<<1>>|r has been removed from the group.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_MEMBER_KICKED_SELF",                "You have been removed from the group.")
+ZO_CreateStringId("SI_LUIE_CA_GROUP_DISBAND_MSG",                       "The group has been disbanded.")
+ZO_CreateStringId("SI_LUIE_CA_TRADE_INVITE_MSG_SELF",                   "You have invited |cFEFEFE<<1>>|r to trade.")
+ZO_CreateStringId("SI_LUIE_CA_TRADE_INVITE_MSG",                        "|cFEFEFE<<1>>|r has invited you to trade.")
+ZO_CreateStringId("SI_LUIE_CA_CANT_THINK_OF_NAME_MSG1",                 "<<1>> and <<2>> gold.") -- TODO: Find proper ID name
+ZO_CreateStringId("SI_LUIE_CA_CANT_THINK_OF_NAME_MSG2",                 "<<1>> and gold.") -- TODO: Find proper ID name
+ZO_CreateStringId("SI_LUIE_CA_MAIL_RECEIVED_GOLD_MSG",                  "Received mail with gold.")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_RECEIVED_VAR_GOLD_MSG",              "Received mail with <<1>> gold.")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_GOLD_SENT",                      "COD Payment sent!")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_VAR_GOLD_SENT1",                 "COD Payment of <<1>> gold sent!")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_VAR_GOLD_SENT2",                 "COD sent for <<1>> gold!")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_SENT_SUCCESS",                       "Mail sent!")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_COD_SENT_SUCCESS",                   "COD sent!")
+ZO_CreateStringId("SI_LUIE_CA_MAIL_SENT_VAR_GOLD_MSG",                  "Mail sent with <<1>> gold!")
+ZO_CreateStringId("SI_LUIE_CA_XP_LVL_ANNOUNCE1",                        "You have reached <<1>><<2>>")
+ZO_CreateStringId("SI_LUIE_CA_XP_LVL_ANNOUNCE2",                        "You have reached <<1>><<2>> <<3>>")
+ZO_CreateStringId("SI_LUIE_CA_CP_LVL_ANNOUNCE1",                        "Champion Level Achieved! <<1>><<2>>")
+ZO_CreateStringId("SI_LUIE_CA_CP_LVL_ANNOUNCE2",                        "Champion Level Achieved! <<1>><<2>> <<3>>")
 
 -- Default overwrites
 ZO_CreateStringId("SI_LUIE_GROUPINVITERESPONSE0",                       "Could not find a player named |cFEFEFE\"<<1>>\"|r to invite.")
@@ -71,5 +56,3 @@ ZO_CreateStringId("SI_LUIE_FRIENDS_LIST_FRIEND_CHARACTER_LOGGED_OFF",   "|cFEFEF
 ZO_CreateStringId("SI_LUIE_PLAYER_TO_PLAYER_INCOMING_GUILD_REQUEST",    "<<1>> has invited you to join <<2>>")
 ZO_CreateStringId("SI_LUIE_GUILD_ROSTER_INVITED_MESSAGE",               "You have invited \"|cffffff<<1>>|r\" to join |cffffff<<2>>|r")
 ZO_CreateStringId("SI_LUIE_PLAYER_TO_PLAYER_INCOMING_QUEST_SHARE",      "<<1>> wants to share the quest: <<2>>.")
-
-
