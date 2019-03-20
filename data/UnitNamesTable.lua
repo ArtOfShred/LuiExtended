ebd87c9e85bb558e4d8b3b0ef2ab36cbb7dc380c
psypanda <psypanda@users.noreply.github.com>
2019-03-20 18:31:21 +0100

    UnitNamesTable restructure

--- data/UnitNamesTable.lua
+++ data/UnitNamesTable.lua
@@ -2,138 +2,140 @@
 -- UnitNames namespace
 LUIE.UnitNames = {}
 
--- Performance Enhancement
-local U = LUIE.UnitNames
+local UnitNames = {
+    -- TODO: MAYBE in the future can localize these names, for the time being this table exists for that purpose.
 
--- TODO: MAYBE in the future can localize these names, for the time being this table exists for that purpose.
+    ----------------------------------------------------------------
+    -- NORMAL NPC NAMES --------------------------------------------
+    ----------------------------------------------------------------
 
-----------------------------------------------------------------
--- NORMAL NPC NAMES --------------------------------------------
-----------------------------------------------------------------
+    NPC_Flame_Atronach                    = 'Flame Atronach',
+    NPC_Frost_Atronach                    = 'Frost Atronach',
+    NPC_Ice_Barrier                       = 'Ice Barrier',
 
-U.NPC_Flame_Atronach                    = 'Flame Atronach'
-U.NPC_Frost_Atronach                    = 'Frost Atronach'
-U.NPC_Ice_Barrier                       = 'Ice Barrier'
-
-----------------------------------------------------------------
--- QUESTS ------------------------------------------------------
-----------------------------------------------------------------
+    ----------------------------------------------------------------
+    -- QUESTS ------------------------------------------------------
+    ----------------------------------------------------------------
 
 
 
-----------------------------------------------------------------
--- WORLD BOSSES ------------------------------------------------
-----------------------------------------------------------------
+    ----------------------------------------------------------------
+    -- WORLD BOSSES ------------------------------------------------
+    ----------------------------------------------------------------
 
 
 
-----------------------------------------------------------------
--- PUBLIC DUNGEONS ---------------------------------------------
-----------------------------------------------------------------
+    ----------------------------------------------------------------
+    -- PUBLIC DUNGEONS ---------------------------------------------
+    ----------------------------------------------------------------
 
 
-----------------------------------------------------------------
--- ARENAS ------------------------------------------------------
-----------------------------------------------------------------
+    ----------------------------------------------------------------
+    -- ARENAS ------------------------------------------------------
+    ----------------------------------------------------------------
 
-U.Boss_Maxus_the_Many                   = 'Maxus the Many'
+    Boss_Maxus_the_Many                   = 'Maxus the Many',
 
-----------------------------------------------------------------
--- DUNGEONS ----------------------------------------------------
-----------------------------------------------------------------
+    ----------------------------------------------------------------
+    -- DUNGEONS ----------------------------------------------------
+    ----------------------------------------------------------------
 
--- Banished Cells I
-U.Boss_Cell_Haunter                     = 'Cell Haunter'
-U.Boss_Shadowrend                       = 'Shadowrend'
-U.Boss_Angata_the_Clannfear_Handler     = 'Angata the Clannfear Handler'
-U.Boss_High_Kinlord_Rilis               = 'High Kinlord Rilis'
+    -- Banished Cells I
+    Boss_Cell_Haunter                     = 'Cell Haunter',
+    Boss_Shadowrend                       = 'Shadowrend',
+    Boss_Angata_the_Clannfear_Handler     = 'Angata the Clannfear Handler',
+    Boss_High_Kinlord_Rilis               = 'High Kinlord Rilis',
 
--- Banished Cells II
-U.Boss_Keeper_Areldur                   = 'Keeper Areldur'
-U.Boss_Maw_of_the_Infernal              = 'Maw of the Infernal'
-U.Boss_Keeper_Voranil                   = 'Keeper Voranil'
-U.Boss_Keeper_Imiril                    = 'Keeper Imiril'
+    -- Banished Cells II
+    Boss_Keeper_Areldur                   = 'Keeper Areldur',
+    Boss_Maw_of_the_Infernal              = 'Maw of the Infernal',
+    Boss_Keeper_Voranil                   = 'Keeper Voranil',
+    Boss_Keeper_Imiril                    = 'Keeper Imiril',
 
--- Elden Hollow I
-U.Boss_Ancient_Spriggan                 = 'Ancient Spriggan'
-U.Boss_Akash_gra_Mal                    = 'Akash gra-Mal'
-U.Boss_Chokethorn                       = 'Chokethorn'
-U.NPC_Strangler_Saplings                = 'Strangler Saplings'
-U.Boss_Nenesh_gro_Mal                   = 'Nenesh gro-Mal'
-U.Boss_Leafseether                      = 'Leafseether'
-U.Boss_Canonreeve_Oraneth               = 'Canonreeve Oraneth'
+    -- Elden Hollow I
+    Boss_Ancient_Spriggan                 = 'Ancient Spriggan',
+    Boss_Akash_gra_Mal                    = 'Akash gra-Mal',
+    Boss_Chokethorn                       = 'Chokethorn',
+    NPC_Strangler_Saplings                = 'Strangler Saplings',
+    Boss_Nenesh_gro_Mal                   = 'Nenesh gro-Mal',
+    Boss_Leafseether                      = 'Leafseether',
+    Boss_Canonreeve_Oraneth               = 'Canonreeve Oraneth',
 
--- Elden Hollow II
-U.Boss_Dubroze_the_Infestor             = 'Dubroze the Infestor'
-U.Boss_Dark_Root                        = 'Dark Root'
-U.NPC_Frenzied_Guardian                 = 'Frenzied Guardian'
-U.NPC_Mystic_Guardian                   = 'Mystic Guardian'
-U.Boss_Azara_the_Frightener             = 'Azara the Frightener'
-U.Boss_Shadow_Tendril                   = 'Shadow Tendril'
-U.Boss_Murklight                        = 'Murklight'
-U.Boss_The_Shadow_Guard                 = 'The Shadow Guard'
-U.Boss_Bogdan_the_Nightflame            = 'Bogdan the Nightflame'
+    -- Elden Hollow II
+    Boss_Dubroze_the_Infestor             = 'Dubroze the Infestor',
+    Boss_Dark_Root                        = 'Dark Root',
+    NPC_Frenzied_Guardian                 = 'Frenzied Guardian',
+    NPC_Mystic_Guardian                   = 'Mystic Guardian',
+    Boss_Azara_the_Frightener             = 'Azara the Frightener',
+    Boss_Shadow_Tendril                   = 'Shadow Tendril',
+    Boss_Murklight                        = 'Murklight',
+    Boss_The_Shadow_Guard                 = 'The Shadow Guard',
+    Boss_Bogdan_the_Nightflame            = 'Bogdan the Nightflame',
 
--- City of Ash I
-U.Boss_Golor_the_Banekin_Handler        = 'Golor the Banekin Handler'
-U.Boss_Warden_of_the_Shrine             = 'Warden of the Shrine'
-U.Boss_Infernal_Guardian                = 'Infernal Guardian'
-U.Boss_Dark_Ember                       = 'Dark Ember'
-U.Boss_Rothariel_Flameheart             = 'Rothariel Flameheart'
-U.Boss_Razor_Master_Erthas              = 'Razor Master Erthas'
+    -- City of Ash I
+    Boss_Golor_the_Banekin_Handler        = 'Golor the Banekin Handler',
+    Boss_Warden_of_the_Shrine             = 'Warden of the Shrine',
+    Boss_Infernal_Guardian                = 'Infernal Guardian',
+    Boss_Dark_Ember                       = 'Dark Ember',
+    Boss_Rothariel_Flameheart             = 'Rothariel Flameheart',
+    Boss_Razor_Master_Erthas              = 'Razor Master Erthas',
 
--- City of Ash II
-U.Boss_Akezel                           = 'Akezel'
-U.Boss_Rukhan                           = 'Rukhan'
-U.Boss_Marruz                           = 'Marruz'
-U.NPC_Xivilai_Immolator                 = 'Xivilai Immolator'
-U.NPC_Xivilai_Ravager                   = 'Xivilai Ravager'
-U.NPC_Venomous_Skeleton                 = 'Venomous Skeleton'
-U.Boss_Urata_the_Legion                 = 'Urata the Legion'
-U.NPC_Flame_Colossus                    = 'Flame Colossus'
-U.Boss_Horvantud_the_Fire_Maw           = 'Horvantud the Fire Maw'
-U.Boss_Ash_Titan                        = 'Ash Titan'
-U.NPC_Air_Atronach                      = 'Air Atronach'
-U.NPC_Dremora_Hauzkyn                   = 'Dremora Hauzkyn'
-U.NPC_Fire_Ravager                      = 'Fire Ravager'
-U.NPC_Xivilai_Fulminator                = 'Xivilai Fulminator'
-U.NPC_Xivilai_Boltaic                   = 'Xivilai Boltaic'
-U.Boss_Valkyn_Skoria                    = 'Valkyn Skoria'
+    -- City of Ash II
+    Boss_Akezel                           = 'Akezel',
+    Boss_Rukhan                           = 'Rukhan',
+    Boss_Marruz                           = 'Marruz',
+    NPC_Xivilai_Immolator                 = 'Xivilai Immolator',
+    NPC_Xivilai_Ravager                   = 'Xivilai Ravager',
+    NPC_Venomous_Skeleton                 = 'Venomous Skeleton',
+    Boss_Urata_the_Legion                 = 'Urata the Legion',
+    NPC_Flame_Colossus                    = 'Flame Colossus',
+    Boss_Horvantud_the_Fire_Maw           = 'Horvantud the Fire Maw',
+    Boss_Ash_Titan                        = 'Ash Titan',
+    NPC_Air_Atronach                      = 'Air Atronach',
+    NPC_Dremora_Hauzkyn                   = 'Dremora Hauzkyn',
+    NPC_Fire_Ravager                      = 'Fire Ravager',
+    NPC_Xivilai_Fulminator                = 'Xivilai Fulminator',
+    NPC_Xivilai_Boltaic                   = 'Xivilai Boltaic',
+    Boss_Valkyn_Skoria                    = 'Valkyn Skoria',
 
--- Tempest Island
-U.Boss_Sonolia_the_Matriarch            = 'Sonolia the Matriarch'
-U.Boss_Valaran_Stormcaller              = 'Valaran Stormcaller'
-U.NPC_Lightning_Avatar                  = 'Lightning Avatar'
-U.Boss_Yalorasse_the_Speaker            = 'Yalorasse the Speaker'
-U.Boss_Stormfist                        = 'Stormfist'
-U.Boss_Commodore_Ohmanil                = 'Commodore Ohmanil'
-U.Boss_Stormreeve_Neidir                = 'Stormreeve Neidir'
+    -- Tempest Island
+    Boss_Sonolia_the_Matriarch            = 'Sonolia the Matriarch',
+    Boss_Valaran_Stormcaller              = 'Valaran Stormcaller',
+    NPC_Lightning_Avatar                  = 'Lightning Avatar',
+    Boss_Yalorasse_the_Speaker            = 'Yalorasse the Speaker',
+    Boss_Stormfist                        = 'Stormfist',
+    Boss_Commodore_Ohmanil                = 'Commodore Ohmanil',
+    Boss_Stormreeve_Neidir                = 'Stormreeve Neidir',
 
--- Selene's Web
-U.Boss_Treethane_Kerninn                = 'Treethane Kerninn'
-U.Boss_Longclaw                         = 'Longclaw'
-U.Boss_Queen_Aklayah                    = 'Queen Aklayah'
-U.Boss_Foulhide                         = 'Foulhide'
-U.Boss_Mennir_Many_Legs                 = 'Mennir Many-Legs'
-U.Boss_Selene                           = 'Selene'
+    -- Selene's Web
+    Boss_Treethane_Kerninn                = 'Treethane Kerninn',
+    Boss_Longclaw                         = 'Longclaw',
+    Boss_Queen_Aklayah                    = 'Queen Aklayah',
+    Boss_Foulhide                         = 'Foulhide',
+    Boss_Mennir_Many_Legs                 = 'Mennir Many-Legs',
+    Boss_Selene                           = 'Selene',
 
--- Spindleclutch I
-U.Boss_Spindlekin                       = 'Spindlekin'
-U.Boss_Swarm_Mother                     = 'Swarm Mother'
-U.Boss_Cerise_the_Widow_Maker           = 'Cerise the Widow-Maker'
-U.Boss_Big_Rabbu                        = 'Big Rabbu'
-U.Boss_The_Whisperer                    = 'The Whisperer'
+    -- Spindleclutch I
+    Boss_Spindlekin                       = 'Spindlekin',
+    Boss_Swarm_Mother                     = 'Swarm Mother',
+    Boss_Cerise_the_Widow_Maker           = 'Cerise the Widow-Maker',
+    Boss_Big_Rabbu                        = 'Big Rabbu',
+    Boss_The_Whisperer                    = 'The Whisperer',
 
-U.Boss_Praxin_Douare                    = 'Praxin Douare'
+    Boss_Praxin_Douare                    = 'Praxin Douare',
 
--- Spindleclutch II
-U.Boss_Mad_Mortine                      = 'Mad Mortine'
-U.Boss_Blood_Spawn                      = 'Blood Spawn'
-U.NPC_The_Whisperer_Nightmare           = 'The Whisperer Nightmare'
-U.NPC_Flesh_Atronach                    = 'Flesh Atronach'
-U.Boss_Urvan_Veleth                     = 'Urvan Veleth'
-U.Boss_Vorenor_Winterbourne             = 'Vorenor Winterbourne'
+    -- Spindleclutch II
+    Boss_Mad_Mortine                      = 'Mad Mortine',
+    Boss_Blood_Spawn                      = 'Blood Spawn',
+    NPC_The_Whisperer_Nightmare           = 'The Whisperer Nightmare',
+    NPC_Flesh_Atronach                    = 'Flesh Atronach',
+    Boss_Urvan_Veleth                     = 'Urvan Veleth',
+    Boss_Vorenor_Winterbourne             = 'Vorenor Winterbourne',
 
--- Frostvault
-U.NPC_Coldsnap_Ogre                     = 'Coldsnap Ogre'
-U.Boss_Icestalker                       = 'Icestalker'
+    -- Frostvault
+    NPC_Coldsnap_Ogre                     = 'Coldsnap Ogre',
+    Boss_Icestalker                       = 'Icestalker',
+}
+
+-- Export string data to global namespace
+LUIE.UnitNames = UnitNames
