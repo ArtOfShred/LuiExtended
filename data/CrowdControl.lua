c4a75b78cbbc0184d9e793d40cb077dac89056de
Anthony S. Castanza <acastanza@ucsd.edu>
2023-08-12 20:57:05 -0700

    Blacklist "Diseased" status effect
    This is not a CC, or AOE but appears to routinely appear in the CC tracker.
    
    Starting a logging project to blacklist misbehaving effects in PVP.

--- data/CrowdControl.lua
+++ data/CrowdControl.lua
@@ -667,6 +667,9 @@ CrowdControl.IgnoreList ={
     -- Frostvault
     [109838] = true, -- End Stun (Icestalker) -- Frostvault
     [119461] = true, -- Teleport Failsafe (Border Chk) -- Frostvault
+
+    -- PVP
+    [178127] = true, -- Diseased
 }
 
 -- Not implemented - list of abilities that ACTION_RESULT_GAINED_DURATION fires in the opposite order of normal
