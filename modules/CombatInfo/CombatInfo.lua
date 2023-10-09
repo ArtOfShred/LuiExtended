1dbad3f146b09e05c23cf76cbec6e0db81da9012
Shredmaster Reeman <artofshred@artofshred.net>
2023-10-09 15:32:31 -0400

    Update CombatInfo.lua

--- modules/CombatInfo/CombatInfo.lua
+++ modules/CombatInfo/CombatInfo.lua
@@ -2318,7 +2318,7 @@ function CombatInfo.BarSlotUpdate(slotNum, wasfullUpdate, onlyProc)
 
     -- TODO: Removed in a previous update but this is important for displaying fake auras for abilities that need them.
     -- Added this back commented out for now since I don't have the time to update the override tables right now.
-    --local showFakeAura = (Effects.BarHighlightOverride[ability_id] and Effects.BarHighlightOverride[ability_id].showFakeAura)
+    -- local showFakeAura = (Effects.BarHighlightOverride[ability_id] and Effects.BarHighlightOverride[ability_id].showFakeAura)
 
     if Effects.BarHighlightOverride[ability_id] then
         if Effects.BarHighlightOverride[ability_id].hide then
