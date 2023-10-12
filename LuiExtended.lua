568b94d6ea66d4d37169156fbfa5b39b28a123ba
Cullen <dakjaniels@outlook.com>
2023-10-12 07:34:27 -0500

    bump

--- LuiExtended.lua
+++ LuiExtended.lua
@@ -10,7 +10,7 @@ end
 
 LUIE.tag = "LUIE"
 LUIE.name = "LuiExtended"
-LUIE.version = "6.5.2"
+LUIE.version = "6.5.3"
 LUIE.author = "ArtOfShred, psypanda, SpellBuilder & Saenic"
 LUIE.website = "https://www.esoui.com/downloads/info818-LuiExtended.html"
 LUIE.github = "https://github.com/ArtOfShred/LuiExtended"
