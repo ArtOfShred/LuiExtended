06fdfc0b5cd6f1a8b033df8e2f1e4d3eff53c4f1
Cullen <dakjaniels@outlook.com>
2023-11-04 08:42:05 -0500

    colors?

--- modules/InfoPanel/InfoPanel.lua
+++ modules/InfoPanel/InfoPanel.lua
@@ -18,13 +18,13 @@ local string_format = string.format
 local moduleName = LUIE.name .. "InfoPanel"
 
 local colors = {
-    RED = { r = 1, g = 0, b = 0 },
-    GREEN = { r = 0, g = 1, b = 0 },
-    BLUE = { r = 0, g = 0, b = 1 },
-    YELLOW = { r = 1, g = 1, b = 0 },
-    WHITE = { r = 1, g = 1, b = 1 },
+    RED = { r = 255, g = 0, b = 0 },
+    GREEN = { r = 0, g = 255, b = 0 },
+    BLUE = { r = 0, g = 0, b = 255 },
+    YELLOW = { r = 255, g = 255, b = 0 },
+    WHITE = { r = 255, g = 255, b = 255 },
     BLACK = { r = 0, g = 0, b = 0 },
-    GRAY = { r = 0.5, g = 0.5, b = 0.5 },
+    GRAY = { r = 128, g = 128, b = 128 },
 }
 
 --local fakeControl   = {}
