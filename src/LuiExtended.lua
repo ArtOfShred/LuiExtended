5a3af3c2d54d96497541584faaf3ec8bf7a994de
Cullen Daugherty <107732615+DakJaniels@users.noreply.github.com>
2024-06-02 21:00:59 -0500

    version bump

--- src/LuiExtended.lua
+++ src/LuiExtended.lua
@@ -12,7 +12,7 @@ local LUIE        = LUIE
 
 LUIE.tag          = "LUIE"
 LUIE.name         = "LuiExtended"
-LUIE.version      = "6.7.0"
+LUIE.version      = "6.7.1"
 LUIE.author       = "ArtOfShred, DakJaniels, psypanda, Saenic & SpellBuilder"
 LUIE.website      = "https://www.esoui.com/downloads/info818-LuiExtended.html"
 LUIE.github       = "https://github.com/ArtOfShred/LuiExtended"
