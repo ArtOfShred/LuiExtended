06914b29827ad1e247479fd02678fdbd39ed6124
Anthony S. Castanza <acastanza@gmail.com>
2024-07-09 07:17:05 -0700

    Added Warden AOE pre-Charm AoE to AoE alerts (#297)

--- data/CrowdControl.lua
+++ data/CrowdControl.lua
@@ -90,6 +90,12 @@ CrowdControl.aoePlayerNormal = {
     [32711] = 1, -- Eruption (Eruption)
 
     -- Warden
+    [220630] = 1, -- Warden's Burst (Warden Scribing Class Charm Initial Cast)
+    [220639] = 0, -- Warden's Burst (Warden Scribing Class Charm Initial Cast)
+
+    [221173] = 1, -- Warden's Contingency (Warden Scribing Class Charm Initial Cast)
+    [221174] = 0, -- Warden's Contingency (Warden Scribing Class Charm Initial Cast)
+
     --[130406] = 0, -- Arctic Blast (Arctic Blast)
     [88783] = 1, -- Impaling Shards (Impaling Shards)
     [88791] = 0, -- Gripping Shards (Gripping Shards)
